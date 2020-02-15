ad34ca5d254e2b8a7258ef4aebbf99f1a0f73410
Nemanja M <manotnemanja@gmail.com>
2020-02-15 20:04:16 +0100

    Created mobx store & initial movies render

--- src/screens/Main/MovieSingle/MovieSingle.js
+++ src/screens/Main/MovieSingle/MovieSingle.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { inject, observer } from "mobx-react";
+import { toJS } from "mobx";
 
-const MovieSingle = () => {
+const MovieSingle = ({ MovieStore }) => {
+    const { singleMovie, isLoading } = toJS(MovieStore);
+
     return (
         <View style={ { flex: 1 } }>
-            <Text>Movie Single screen</Text>
+            <Text>{ singleMovie.title }</Text>
         </View>
     )
 };
 
-export default MovieSingle;
+export default inject("MovieStore")(observer(MovieSingle));
--- src/screens/Main/Movies/Movies.js
+++ src/screens/Main/Movies/Movies.js
@@ -1,20 +1,42 @@
-import React from "react";
-import { View, Text, Button as RNButton } from "react-native";
-import { Button } from "react-native-paper";
+import React, { useEffect } from "react";
+import { ScrollView, Button as RNButton, Image } from "react-native";
+import { toJS } from "mobx";
+import { observer, inject } from "mobx-react";
+import { Card } from "react-native-paper";
 
-const Movies = ({ navigation }) => {
-    const navigateToSingleMovie = () => {
-        navigation.navigate("MovieSingle");
+const Movies = ({ navigation, MovieStore }) => {
+    useEffect(() => {
+        MovieStore.getNowPlayingMovies();
+    }, [MovieStore]);
+
+    const { movies, isLoading } = toJS(MovieStore);
+
+    const onMoviePress = async (id) => {
+        await MovieStore.getSingleMovie(id);
+        await navigation.navigate("MovieSingle");
+    };
+
+    const renderMovie = (movie) => {
+        const { id, title, release_date, poster_path } = movie;
+        const imageUri = `https://image.tmdb.org/t/p/w500/${ poster_path }`;
+        return (
+            <Card
+                key={ id }
+                onPress={ onMoviePress.bind(this, id) }
+            >
+                <Card.Title
+                    title={ title }
+                    subtitle={ release_date }
+                />
+                <Image style={ { height: 200, width: 150 } } source={ { uri: imageUri } }/>
+            </Card>
+        )
     };
 
     return (
-        <View style={ { flex: 1 } }>
-            <Text>Movies screen</Text>
-
-            <Button mode="contained" onPress={ navigateToSingleMovie } style={ { marginTop: 10 } }>
-                Go to single movie
-            </Button>
-        </View>
+        <ScrollView style={ { flex: 1 } }>
+            { movies.map(movie => renderMovie(movie)) }
+        </ScrollView>
     )
 };
 
@@ -28,4 +50,4 @@ Movies.navigationOptions = ({ navigation }) => ({
     )
 });
 
-export default Movies;
+export default inject("MovieStore")(observer(Movies));
